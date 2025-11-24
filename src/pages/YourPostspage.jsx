1d37bb03f7962b73e00091963885140ac080d920
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-11-24 17:31:17 +1030

    wrapped YourPosts in container for better ui

--- src/pages/YourPostspage.jsx
+++ src/pages/YourPostspage.jsx
@@ -1,13 +1,19 @@
 import YourPosts from "@/components/YourPosts";
 import React from 'react'
+import { Container } from "@mui/material";
 
 function YourPostspage() {
-  return (
-    <div>
-        <YourPosts/>
-      
-    </div>
-  )
+    return (
+        <>
+            <Container maxWidth="md" sx={{ mt: 2 }}>
+
+                <YourPosts />
+            </Container>
+        </>
+
+
+
+    )
 }
 
 export default YourPostspage

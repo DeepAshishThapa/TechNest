466bbd6472b2d7b5dafdf205fa9a063f825cbd8c
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-11-24 15:22:05 +1030

    made YourPost page to wrap users posts

---
+++ src/pages/YourPostspage.jsx
@@ -0,0 +1,13 @@
+import YourPosts from "@/components/YourPosts";
+import React from 'react'
+
+function YourPostspage() {
+  return (
+    <div>
+        <YourPosts/>
+      
+    </div>
+  )
+}
+
+export default YourPostspage
